5b95fb4bd312e5467c3700c7af45b43655f78d99
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-12 00:37:32 +0200

    updated

--- Calculatrice.cs
+++ Calculatrice.cs
@@ -4,34 +4,38 @@ namespace TPCalculatrice
 {
     public class Calculatrice
     {
-        public int Addition(int a, int b)
+
+        public int OperanteGauche { get; set; }
+        public int OperanteDroite { get; set; }
+        public int Resultat { get; set; }
+
+        public void Addition()
         {
-            return a + b;
+            Resultat = OperanteGauche + OperanteDroite;
         }
 
-        public int Soustraction(int a, int b)
+        public void Soustraction()
         {
-            return a - b;
+            Resultat = OperanteGauche - OperanteDroite;
         }
 
-        public int Multiplication(int a, int b)
+        public void Multiplication()
         {
-            return a * b;
+            Resultat = OperanteGauche * OperanteDroite;
         }
 
-        public int Division(int a, int b)
+        public void Division()
         {
-            int resu = 0;
-            if (b != 0)
+            Resultat = 0;
+            if (OperanteDroite != 0)
             {
-                resu = a / b;
+                Resultat = OperanteGauche / OperanteDroite;
             }
-            return resu;
         }
 
-        public int Modulo(int a, int b)
+        public void Modulo()
         {
-            return a % b;
+            Resultat = OperanteGauche % OperanteDroite;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,35 +5,34 @@ Console.WriteLine("======== CALCULATRICE ========");
 Calculatrice maCalc = new Calculatrice();
 
 Console.WriteLine("Veuillez saisir un premier chiffre : ");
-int a = int.Parse(Console.ReadLine());
+maCalc.OperanteGauche = int.Parse(Console.ReadLine());
 Console.WriteLine("Veuillez saisir un deuxièmre chiffre : ");
-int b = int.Parse(Console.ReadLine());
+maCalc.OperanteDroite = int.Parse(Console.ReadLine());
 Console.WriteLine();
 
 Console.WriteLine("Saisir un opérateur entre + - * / % : ");
 string op = Console.ReadLine();
 
-int resu = 0;
 switch (op)
 {
     case "+":
-        resu = maCalc.Addition(a, b);
+        maCalc.Addition();
         break;
     case "-":
-        resu = maCalc.Soustraction(a, b);
+        maCalc.Soustraction();
         break;
     case "*":
-        resu = maCalc.Multiplication(a, b);
+        maCalc.Multiplication();
         break;
     case "/":
-        resu = maCalc.Division(a, b);
+        maCalc.Division();
         break;
     case "%":
-        resu = maCalc.Modulo(a, b);
+        maCalc.Modulo();
         break;
     default:
         Console.WriteLine("Opérateur incorrect");
         break;
 }
 
-Console.WriteLine($"Résultat : {resu}");
+Console.WriteLine($"Résultat : {maCalc.Resultat}");
