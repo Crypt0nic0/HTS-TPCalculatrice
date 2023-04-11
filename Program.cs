732ab1c8fce1150cdd25189dc7c19fcfb1765c73
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-12 00:16:51 +0200

    calc testing finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("======== CALCULATRICE ========");
+
+TPCalculatrice.Calculatrice maCalc = new TPCalculatrice.Calculatrice();
+
+Console.WriteLine("Veuillez saisir un premier chiffre : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Veuillez saisir un deuxièmre chiffre : ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Saisir un opérateur entre + - * / % : ");
+string op = Console.ReadLine();
+
+int resu = 0;
+switch (op)
+{
+    case "+":
+        resu = maCalc.Addition(a, b);
+        break;
+    case "-":
+        resu = maCalc.Soustraction(a, b);
+        break;
+    case "*":
+        resu = maCalc.Multiplication(a, b);
+        break;
+    case "/":
+        resu = maCalc.Division(a, b);
+        break;
+    case "%":
+        resu = maCalc.Modulo(a, b);
+        break;
+    default:
+        Console.WriteLine("Opérateur incorrect");
+        break;
+}
+
+Console.WriteLine($"Résultat : {resu}");
