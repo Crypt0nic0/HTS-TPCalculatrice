6a80881eeaeb667baab1a1dee77a3936c11811ae
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-12 11:33:37 +0200

    updated

--- Operations/Addition.cs
+++ Operations/Addition.cs
@@ -2,10 +2,15 @@ using System;
 
 namespace TPCalculatrice.Operations
 {
+    // Classe Addition qui hérite de Opération
     public class Addition : Operation
     {
+        // Pour instancier une Addition, on doit lui donner les 2 opérateurs...
+        // qui sont necessaires dans l'Operation
         public Addition(int o1, int o2) : base(o1, o2) { }
 
+        // La méthode Executer est abstract dans Operation, elle doit donc...
+        // être redéfinie dans les héritiers.
         public override void Executer()
         {
             Resultat = OperanteGauche + OperanteDroite;
--- Operations/Operation.cs
+++ Operations/Operation.cs
@@ -4,17 +4,24 @@ namespace TPCalculatrice.Operations
 {
     public abstract class Operation
     {
+        // Une Operation a besoin de 2 operante
+        // Elle fournira un Resultat
         public int OperanteGauche { get; set; }
         public int OperanteDroite { get; set; }
 
         public int Resultat { get; set; }
 
+
+        // Pour instancier une Operation...
+        // il faut donc lui donner les 2 operantes
         public Operation(int operanteGauche, int operanteDroite)
         {
             OperanteGauche = operanteGauche;
             OperanteDroite = operanteDroite;
         }
 
+        // Une méthode Executer pour executer l'operation
+        // Methode Abstract, elle doit donc être forcément re-définie dans les héritiers
         public abstract void Executer();
 
     }
